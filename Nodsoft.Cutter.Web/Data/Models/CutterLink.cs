ad3215508063abd7577545c178e75b9ea4ffd191
DJ Daemonix <dj.daemonix@nodsoft.net>
2021-02-01 13:09:11 +0100

    Draft Data Source + Models

---
+++ Nodsoft.Cutter.Web/Data/Models/CutterLink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nodsoft.Cutter.Web.Data.Models
+{
+	public class CutterLink
+	{
+		[BsonId, BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; init; }
+
+		public string Name { get; set; }
+
+		[Required, Url]
+		public string Destination { get; set; }
+
+		public DateTime CreatedAt { get; init; }
+		public string CreatedFromIp { get; set; }
+	}
+}
